12ed7bda2c18724d099c111a95b100f826cc99da
anamerasul <anamerasul@gmail.com>
2022-02-13 20:52:27 +0600

    pin

--- app.js
+++ app.js
@@ -25,6 +25,59 @@ function getpin(){
 
 
 function generatepin(){
-        getpin()
+       const pin= getpin();
+
+        document.getElementById('display-pin').value=pin;
         
+}
+
+document.getElementById('key-pad').addEventListener('click',function(event){
+//       console.log(event.target.innerText)
+      
+      const number=event.target.innerText;
+
+      const calcInput= document.getElementById('typed-numbers');
+
+      if(isNaN(number)){
+              console.log(number)
+
+              if(number=='C'){
+                calcInput.value="";     
+              }
+      }
+
+      else{
+
+//      const calcInput= document.getElementById('typed-numbers');
+     const previouscalc=calcInput.value
+     const newcalc= previouscalc+number
+//      calc.value=number
+     calcInput.value=newcalc
+
+      }
+} );
+
+
+function verifypin(){
+
+       const pin=document.getElementById('display-pin');
+
+       const generatePin=pin.value;
+
+       const typedpin=document.getElementById('typed-numbers');
+
+       const typedpinValue=typedpin.value;
+const successMessage=  document.getElementById('notify-pass')
+
+const failedmessage= document.getElementById('notify-failed')
+       if(generatePin==typedpinValue){
+        successMessage.style.display='block'
+        failedmessage.style.display='none';
+       }
+
+       else{
+        successMessage.style.display='none'
+        failedmessage.style.display='block';
+       }
+
 }
